2b341097a32fbaf25e610b71a906faf1573f7986
Simone <info@simoneicardi.com>
2026-05-02 11:56:36 +0200

    aggiungi axios e prima parte setup chiamata

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,55 +1,70 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import BookCard from "../components/BookCard";
 
-const tempBooks = [
-    {
-        "id": 1,
-        "title": "The Great Gatsby",
-        "author": "F. Scott Fitzgerald",
-        "abstract": "A novel about the American dream and the tragedy of idealism.",
-        "image": "great-gatsby.jpg",
-        "created_at": "2024-11-26T09:58:09.000Z",
-        "updated_at": "2024-12-20T10:30:54.000Z"
-    },
-    {
-        "id": 2,
-        "title": "To Kill a Mockingbird",
-        "author": "Harper Lee",
-        "abstract": "A story of racial injustice and moral growth in the Deep South.",
-        "image": "to_kill_a_mocking_bird.jpg",
-        "created_at": "2024-11-26T09:58:09.000Z",
-        "updated_at": "2024-12-20T10:30:54.000Z"
-    },
-    {
-        "id": 3,
-        "title": "1984",
-        "author": "George Orwell",
-        "abstract": "A dystopian tale of a totalitarian regime and the fight for freedom.",
-        "image": "1984.jpg",
-        "created_at": "2024-11-26T09:58:09.000Z",
-        "updated_at": "2024-12-20T10:30:54.000Z"
-    },
-    {
-        "id": 4,
-        "title": "Pride and Prejudice",
-        "author": "Jane Austen",
-        "abstract": "A romantic story about love and social standings.",
-        "image": "pride_and_prejudice.jpg",
-        "created_at": "2024-11-26T09:58:09.000Z",
-        "updated_at": "2024-12-20T10:28:01.000Z"
-    },
-    {
-        "id": 5,
-        "title": "Moby Dick",
-        "author": "Herman Melville",
-        "abstract": "A tale of obsession and revenge on the high seas.",
-        "image": "moby_dick.jpg",
-        "created_at": "2024-11-26T09:58:09.000Z",
-        "updated_at": "2024-12-20T10:30:54.000Z"
-    }
-];
+
+// const tempBooks = [
+//     {
+//         "id": 1,
+//         "title": "The Great Gatsby",
+//         "author": "F. Scott Fitzgerald",
+//         "abstract": "A novel about the American dream and the tragedy of idealism.",
+//         "image": "great-gatsby.jpg",
+//         "created_at": "2024-11-26T09:58:09.000Z",
+//         "updated_at": "2024-12-20T10:30:54.000Z"
+//     },
+//     {
+//         "id": 2,
+//         "title": "To Kill a Mockingbird",
+//         "author": "Harper Lee",
+//         "abstract": "A story of racial injustice and moral growth in the Deep South.",
+//         "image": "to_kill_a_mocking_bird.jpg",
+//         "created_at": "2024-11-26T09:58:09.000Z",
+//         "updated_at": "2024-12-20T10:30:54.000Z"
+//     },
+//     {
+//         "id": 3,
+//         "title": "1984",
+//         "author": "George Orwell",
+//         "abstract": "A dystopian tale of a totalitarian regime and the fight for freedom.",
+//         "image": "1984.jpg",
+//         "created_at": "2024-11-26T09:58:09.000Z",
+//         "updated_at": "2024-12-20T10:30:54.000Z"
+//     },
+//     {
+//         "id": 4,
+//         "title": "Pride and Prejudice",
+//         "author": "Jane Austen",
+//         "abstract": "A romantic story about love and social standings.",
+//         "image": "pride_and_prejudice.jpg",
+//         "created_at": "2024-11-26T09:58:09.000Z",
+//         "updated_at": "2024-12-20T10:28:01.000Z"
+//     },
+//     {
+//         "id": 5,
+//         "title": "Moby Dick",
+//         "author": "Herman Melville",
+//         "abstract": "A tale of obsession and revenge on the high seas.",
+//         "image": "moby_dick.jpg",
+//         "created_at": "2024-11-26T09:58:09.000Z",
+//         "updated_at": "2024-12-20T10:30:54.000Z"
+//     }
+// ];
 
 
 function HomePage() {
+
+    // creiamo la var di stato
+    const [books, setBooks] = useState([]);
+
+    // definizione funzione per chiamata ajax a BE
+    const fetchBooks = () => {
+        axios.get("http://localhost:3000/api/books")
+            .then(response => { setBooks(response.data) })
+            .catch(err => {
+                console.log(err);
+            })
+    }
 
     return (
         <>
