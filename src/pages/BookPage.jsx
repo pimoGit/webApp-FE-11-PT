bfa4b235384ed60e5507b6fe2a577df7b22f818a
Simone <info@simoneicardi.com>
2026-05-02 12:25:25 +0200

    logica base chiamata in bookPage

--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -1,72 +1,51 @@
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 import ReviewCard from "../components/ReviewCard";
-
-const tempBookDetail = {
-    "id": 3,
-    "title": "1984",
-    "author": "George Orwell",
-    "abstract": "A dystopian tale of a totalitarian regime and the fight for freedom.",
-    "image": "1984.jpg",
-    "created_at": "2024-11-26T09:58:09.000Z",
-    "updated_at": "2024-12-20T10:30:54.000Z",
-    "reviews": [
-        {
-            "id": 7,
-            "book_id": 3,
-            "name": "Grace",
-            "vote": 5,
-            "text": "A chilling and relevant story, even today.",
-            "created_at": "2024-11-26T09:58:09.000Z",
-            "updated_at": "2024-11-26T09:58:09.000Z"
-        },
-        {
-            "id": 8,
-            "book_id": 3,
-            "name": "Hank",
-            "vote": 4,
-            "text": "Dark and thought-provoking.",
-            "created_at": "2024-11-26T09:58:09.000Z",
-            "updated_at": "2024-11-26T09:58:09.000Z"
-        },
-        {
-            "id": 9,
-            "book_id": 3,
-            "name": "Ivy",
-            "vote": 3,
-            "text": "Too bleak for my taste, but well written.",
-            "created_at": "2024-11-26T09:58:09.000Z",
-            "updated_at": "2024-11-26T09:58:09.000Z"
-        },
-        {
-            "id": 20,
-            "book_id": 3,
-            "name": "newMarioNN",
-            "vote": 5,
-            "text": "bello peccato che è così che sta andando il mondo",
-            "created_at": "2026-02-25T11:25:23.000Z",
-            "updated_at": "2026-02-25T11:25:23.000Z"
-        }
-    ]
-};
 
 
 function BookPage() {
 
+    // recuperiamo id da param dinamico
+    const { id } = useParams();
+
+    // set di var di stato
+    const [book, setBook] = useState({});
+
+    // definiamo funzione chiamata a BE
+    const fetchBook = () => {
+        axios.get("http://localhost:3000/api/books/" + id)
+            .then(response => { setBook(response.data) })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    // definizione funzione rendering reviews
+    const renderReview = () => {
+        return
+    }
+
+    // chiamata da useEffect
+    useEffect(fetchBook, []);
+
     return (
         <>
             <header id="book" className="border-bottom border-1 mb-3">
-                <h1>{tempBookDetail.title}</h1>
-                <h3 className="text-muted"><i>By Nome autore</i></h3>
-                <p>lorem ipsm dolor sit amet</p>
+                <div className="d-flex mb-3">
+                    {book?.image && <img className="book-img" src={book?.image} alt={book?.title} />}
+                </div>
+
+                <h1>{book?.title}</h1>
+                <h3 className="text-muted"><i>By {book?.author}</i></h3>
+                <p>{book?.abstract}</p>
             </header>
             <section id="reviews">
                 <header className="d-flex justify-content-between align-items-center mb-4">
                     <h4>Our community reviews</h4>
                 </header>
-                <ReviewCard key={tempBookDetail.reviews[0].id} review={tempBookDetail.reviews[0]} />
-                <ReviewCard key={tempBookDetail.reviews[1].id} review={tempBookDetail.reviews[1]} />
-                <ReviewCard key={tempBookDetail.reviews[2].id} review={tempBookDetail.reviews[2]} />
+                {renderReview()}
 
             </section>
             <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,55 +3,6 @@ import { useState, useEffect } from "react";
 import BookCard from "../components/BookCard";
 
 
-// const tempBooks = [
-//     {
-//         "id": 1,
-//         "title": "The Great Gatsby",
-//         "author": "F. Scott Fitzgerald",
-//         "abstract": "A novel about the American dream and the tragedy of idealism.",
-//         "image": "great-gatsby.jpg",
-//         "created_at": "2024-11-26T09:58:09.000Z",
-//         "updated_at": "2024-12-20T10:30:54.000Z"
-//     },
-//     {
-//         "id": 2,
-//         "title": "To Kill a Mockingbird",
-//         "author": "Harper Lee",
-//         "abstract": "A story of racial injustice and moral growth in the Deep South.",
-//         "image": "to_kill_a_mocking_bird.jpg",
-//         "created_at": "2024-11-26T09:58:09.000Z",
-//         "updated_at": "2024-12-20T10:30:54.000Z"
-//     },
-//     {
-//         "id": 3,
-//         "title": "1984",
-//         "author": "George Orwell",
-//         "abstract": "A dystopian tale of a totalitarian regime and the fight for freedom.",
-//         "image": "1984.jpg",
-//         "created_at": "2024-11-26T09:58:09.000Z",
-//         "updated_at": "2024-12-20T10:30:54.000Z"
-//     },
-//     {
-//         "id": 4,
-//         "title": "Pride and Prejudice",
-//         "author": "Jane Austen",
-//         "abstract": "A romantic story about love and social standings.",
-//         "image": "pride_and_prejudice.jpg",
-//         "created_at": "2024-11-26T09:58:09.000Z",
-//         "updated_at": "2024-12-20T10:28:01.000Z"
-//     },
-//     {
-//         "id": 5,
-//         "title": "Moby Dick",
-//         "author": "Herman Melville",
-//         "abstract": "A tale of obsession and revenge on the high seas.",
-//         "image": "moby_dick.jpg",
-//         "created_at": "2024-11-26T09:58:09.000Z",
-//         "updated_at": "2024-12-20T10:30:54.000Z"
-//     }
-// ];
-
-
 function HomePage() {
 
     // creiamo la var di stato
@@ -66,15 +17,26 @@ function HomePage() {
             })
     }
 
+    // definizione funzione rendering per card books
+    const renderBooks = () => {
+        return books.map((book) => {
+            return (
+                <div className="col" key={book.id}>
+                    <BookCard book={book} />
+                </div>
+            )
+        })
+    }
+
+    useEffect(fetchBooks, []);
+
     return (
         <>
             <h1 className="text-primary">Bool Books</h1>
             <h2><i>The nerdest book community</i></h2>
 
             <div className="row row-cols-3 mt-4">
-                <BookCard key={tempBooks[0].id} book={tempBooks[0]} />
-                <BookCard key={tempBooks[1].id} book={tempBooks[1]} />
-                <BookCard key={tempBooks[2].id} book={tempBooks[2]} />
+                {renderBooks()}
             </div>
         </>
     )
